da42d3ef5a49da980b16dc2458fc52a985a936ac
simplenotes <34387860+simplenotes@users.noreply.github.com>
2017-12-09 19:49:06 +0100

    Add files via upload

--- static/js/simplenotes.js
+++ static/js/simplenotes.js
@@ -1,4 +1,10 @@
+
+
+
 var elem = document.getElementById('text_origin');
+var today = new Date();
+var expiry = new Date(today.getTime() + 30 * 24 * 3600 * 1000); // plus 30 days
+
 elem.onkeyup = function(e){
     if(e.keyCode == 13){
        getAndInsert();
@@ -11,22 +17,14 @@ function getAndInsert() {
     div.innerHTML = '<div class="row pb-2">\
                 <div class="col-auto">\
                   <div class="btn-group" role="group">\
-                    <button type="button" class="btn btn-secondary">¡Hecho!</button>\
-                    <div class="btn-group" role="group">\
-                      <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\
-                        ···\
-                      </button>\
-                      <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">\
-                        <button type="button" class="close dropdown-item" data-dismiss="alert" aria-label="Close">\
-                        <a class="dropdown-item" href="#">Eliminar</a>\
-                      </div>\
-                    </div>\
+                    <button type="button" class="btn btn-secondary bg-success" id="buttonDone"><i class="material-icons">check</i></button>\
+                    <button type="button" class="btn btn-secondary bg-danger" id="buttonDelete"><i class="material-icons">close</i></button>\
                   </div>\
                 </div>\
                 <div class="col align-middle">\
                   <div class="card pb-2 pt-1 pl-2">\
                     <div class="card-body container-fluid">\
-                        <p>'+text+'</p>\
+                        <p class="flower">'+text+'</p>\
                     </div>\
                   </div>\
                 </div>\
@@ -34,3 +32,17 @@ function getAndInsert() {
     document.getElementById('todos').appendChild(div);
     document.getElementById('text_origin').value = "";
 }
+
+
+function setCookie(name, value) {
+	document.cookie=name + "=" + escape(value) + "; path=/; expires=" + expiry.toGMTString();
+}
+
+function storeValues(form){
+    setCookie("field1", form.field1.value); //name ="field1"
+    setCookie("field2", form.field2.value);
+    setCookie("field3", form.field3.value);
+    setCookie("field4", form.field4.value);
+    return true;
+  }
+
