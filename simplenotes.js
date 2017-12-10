1cc198f75a41f72676f80dfb0364788f430cfbf9
simplenotes <34387860+simplenotes@users.noreply.github.com>
2017-12-10 22:27:50 +0100

    Update simplenotes.js

--- simplenotes.js
+++ simplenotes.js
@@ -1,7 +1,7 @@
 //Ramon Vila Ferreres 2018 - No Comercial
 //Simplenotes.js
 
-document.addEventListener("DOMContentLoaded", loadAllCookies, false);
+document.addEventListener("DOMContentLoaded", loadAllCookies(), false);
 var cookie_counter = 0;
 var counter = 0;
 var deleted =[]; //Deleted posts
